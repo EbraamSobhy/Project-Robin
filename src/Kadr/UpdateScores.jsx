1790efa102ca0ed0597f52a34311866b4b4aa6fc
Ebraam Sobhy <ibraam.sobhy2003@gmail.com>
2025-07-31 18:10:44 +0300

    fix UI Harvest

--- src/Kadr/UpdateScores.jsx
+++ src/Kadr/UpdateScores.jsx
@@ -26,7 +26,7 @@ function UpdateScores() {
         const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
         link.type = 'image/x-icon';
         link.rel = 'shortcut icon';
-        link.href = '/score.png'; // Use a relevant icon for scores
+        link.href = '/score.png';
         document.getElementsByTagName('head')[0].appendChild(link);
         return () => clearTimeout(timer);
     }, []);
